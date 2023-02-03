fb7849b13abb9e682bcbc46eae3e05e8b95e1e96
임현홍 <bbnerino@gmail.com>
2023-02-03 09:20:19 +0900

    first commit

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AutoAppConfigTest {
-    @Test
+//    @Test
     void basicScan(){
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
         MemberService bean = ac.getBean(MemberService.class);
